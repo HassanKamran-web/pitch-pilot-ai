5e4ad916934f43dec6aeabea7d723b0116fa3729
Hassan <hassankamran508@gmail.com>
2026-04-15 12:40:22 +0500

    fix bugs v1

--- frontend/src/ProtectWrappers/ProtectedRouteWrapper.jsx
+++ frontend/src/ProtectWrappers/ProtectedRouteWrapper.jsx
@@ -1,12 +1,22 @@
 import { Navigate } from "react-router-dom"
 import { useAuth } from "../Context/AuthContext"
 import LoadingScreen from "../Components/LoadingScreen"
+import { useState } from "react"
 
 const ProtectedRouteWrapper = ({ children }) => {
   const { user, loading } = useAuth()
+  const [defaultpage, setDefaultpage] = useState(false)
+  if (!user) {
+    setDefaultpage(true)
+  } else {
+    setDefaultpage(false)
+  }
 
+  if (defaultpage) {
+    return <LoadingScreen />
+  }
   if (loading) {
-    return <LoadingScreen/>
+    return <LoadingScreen />
   }
 
   if (!user) return <Navigate to="/" replace />
