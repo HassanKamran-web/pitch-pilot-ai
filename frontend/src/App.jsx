99face7e07c2a39f5e83d6695d4223ac77c809f5
Hassan <hassankamran508@gmail.com>
2026-04-22 23:02:59 +0500

    fix bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,15 @@
 import React, { lazy, Suspense } from 'react'
 import Navbar from './Components/Navbar'
 import { Route, Routes } from 'react-router-dom'
-// import Login from './Pages/Login'
 const Login = lazy(() => import('./Pages/Login'))
-// import Register from './Pages/Register'
 const Register = lazy(() => import('./Pages/Register'))
-import { Toaster } from 'react-hot-toast'
-// import Dashboard from './Pages/Dashboard'
 const Dashboard = lazy(() => import('./Pages/Dashboard'))
-// import Profile from './Pages/Profile'
 const Profile = lazy(() => import('./Pages/Profile'))
-// import Upload from './Pages/Upload'
 const Upload = lazy(() => import('./Pages/Upload'))
-// import Results from './Pages/Results'
 const Results = lazy(() => import('./Pages/Results'))
-// import Pricing from './Pages/Pricing'
 const Pricing = lazy(() => import('./Pages/Pricing'))
-// import LandingPage from './Pages/LandingPage'
 const LandingPage = lazy(() => import('./Pages/LandingPage'))
+import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from './Context/AuthContext'
 import ProtectedRouteWrapper from './ProtectWrappers/ProtectedRouteWrapper'
 import LoadingRouteWrapper from './ProtectWrappers/LoadingRouteWrapper'
